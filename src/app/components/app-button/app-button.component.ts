474df7eeb4e20c252a1e660da86b5bdcd22e8f52
Trifon Trifonov <trifon.j.trifonov@gmail.com>
2018-04-05 14:39:31 +0300

    create app-button component

---
+++ src/app/components/app-button/app-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-button',
+  templateUrl: './app-button.component.html',
+  styleUrls: ['./app-button.component.scss']
+})
+export class AppButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
