7a03f37ca95959274145200f63562ed0e5217596
Trifon Trifonov <trifon.j.trifonov@gmail.com>
2018-04-05 15:20:38 +0300

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ButtonModule } from 'primeng/button';
 
 import { AppComponent } from './app.component';
 import { ShopyHeaderComponent } from './components/shopy-header/shopy-header.component';
+import { AppButtonComponent } from './components/app-button/app-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShopyHeaderComponent,
-    ShopyHeaderComponent
+    AppButtonComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
